4918da361f1871b1acc7ef7d5a2f5c4654d2af6c
RPL-YuniZanika <yuyunisyerin@gmail.com>
2022-06-23 09:17:01 +0700

    Github Profile

--- app/src/main/java/id/polbeng/yun/girhubprofile/activities/MainActivity.kt
+++ app/src/main/java/id/polbeng/yun/girhubprofile/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.request.RequestOptions
 import id.polbeng.yun.girhubprofile.GlideApp
 import id.polbeng.yun.girhubprofile.R
@@ -12,14 +13,16 @@ import id.polbeng.yun.girhubprofile.helpers.Config
 import id.polbeng.yun.girhubprofile.models.GithubUser
 import id.polbeng.yun.girhubprofile.services.GithubUserService
 import id.polbeng.yun.girhubprofile.services.ServiceBuilder
+import id.polbeng.yun.girhubprofile.viewmodels.MainViewModel
+import id.polbeng.yun.girhubprofile.viewmodels.MainViewModel.Companion.TAG
 import retrofit2.Call
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    companion object {
-        val TAG: String = MainActivity::class.java.simpleName
-    }
+//    companion object {
+//        val TAG: String = MainActivity::class.java.simpleName
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,43 +30,55 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        binding.btnSearchUserLogin.setOnClickListener {
-            val userLogin = binding.etSearchUserLogin.text.toString()
-            if (userLogin.isNotEmpty()) {
-                searchUser(userLogin)
-            }
+        val mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
+            MainViewModel::class.java)
+        mainViewModel.githubUser.observe(this) { user ->
+            setUserData(user)
+        }
+        mainViewModel.isLoading.observe(this) {
+            showLoading(it)
         }
 
-        searchUser(Config.DEFAULT_USER_LOGIN)
-    }
-    private fun searchUser(query: String){
-        showLoading(true)
-        Log.d(TAG, "getDataUserProfileFromAPI: start...")
-        val githubUserService: GithubUserService = ServiceBuilder.buildService(GithubUserService::class.java)
-        val requestCall: Call<GithubUser> = githubUserService.loginUser(query)
-        requestCall.enqueue(object : retrofit2.Callback<GithubUser> {
-            override fun onResponse(call: Call<GithubUser>, response: retrofit2.Response<GithubUser>) {
-                showLoading(false)
-                if(response.isSuccessful){
-                    val result = response.body()
-                    if (result != null) {
-                        setUserData(result)
-                    }
-                    Log.d(TAG, "getDataUserFromAPI: onResponse finish...")
-                }else{
-                    binding.tvUser.text = "User Not Found"
-                    GlideApp.with(applicationContext)
-                        .load(R.drawable.ic_baseline_broken_image_24)
-                        .into(binding.imgUser)
-                    Log.d(TAG, "getDataUserFromAPI: onResponse failed...")
-                }
+        binding.btnSearchUserLogin.setOnClickListener {
+            val userLogin = binding.etSearchUserLogin.text.toString()
+            var query = Config.DEFAULT_USER_LOGIN
+            if (userLogin.isNotEmpty()) {
+                query = userLogin
             }
-            override fun onFailure(call: Call<GithubUser>, t: Throwable) {
-                showLoading(false)
-                Log.d(TAG, "getDataUserFromAPI: onFailure ${t.message}...")
-            }
-        })
+            mainViewModel.searchUser(query)
+        }
+
+//        searchUser(Config.DEFAULT_USER_LOGIN)
     }
+//    private fun searchUser(query: String){
+//        showLoading(true)
+//        Log.d(TAG, "getDataUserProfileFromAPI: start...")
+//        val githubUserService: GithubUserService = ServiceBuilder.buildService(GithubUserService::class.java)
+//        val requestCall: Call<GithubUser> = githubUserService.loginUser(query)
+//
+//        requestCall.enqueue(object : retrofit2.Callback<GithubUser> {
+//            override fun onResponse(call: Call<GithubUser>, response: retrofit2.Response<GithubUser>) {
+//                showLoading(false)
+//                if(response.isSuccessful){
+//                    val result = response.body()
+//                    if (result != null) {
+//                        setUserData(result)
+//                    }
+//                    Log.d(TAG, "getDataUserFromAPI: onResponse finish...")
+//                }else{
+//                    binding.tvUser.text = "User Not Found"
+//                    GlideApp.with(applicationContext)
+//                        .load(R.drawable.ic_baseline_broken_image_24)
+//                        .into(binding.imgUser)
+//                    Log.d(TAG, "getDataUserFromAPI: onResponse failed...")
+//                }
+//            }
+//            override fun onFailure(call: Call<GithubUser>, t: Throwable) {
+//                showLoading(false)
+//                Log.d(TAG, "getDataUserFromAPI: onFailure ${t.message}...")
+//            }
+//        })
+//    }
     private fun setUserData(githubUser: GithubUser) {
         binding.tvUser.text = githubUser.toString()
         GlideApp.with(applicationContext)
--- app/src/main/java/id/polbeng/yun/girhubprofile/helpers/Config.kt
+++ app/src/main/java/id/polbeng/yun/girhubprofile/helpers/Config.kt
@@ -1,9 +1,12 @@
 package id.polbeng.yun.girhubprofile.helpers
 
+import id.polbeng.yun.girhubprofile.BuildConfig
+
 class Config {
     companion object{
         const val SPLASH_SCREEN_DELAY:Long = 3000
         const val BASE_URL ="https://api.github.com"
         const val DEFAULT_USER_LOGIN = "YuniZanika"
+        const val PERSONAL_ACCESS_TOKEN = "token ${BuildConfig.ACCESS_TOKEN}"
     }
 }
